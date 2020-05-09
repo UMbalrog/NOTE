00d9563bf446fe2d84dd510eda4d8c27b4b31b8f
xujie <xujie@yixia.com>
2020-05-09 10:17:22 +0800

    新文件

---
+++ Work_item/day20200506uni_app/testproject/src/pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="content">
+		测试文字
+    <div>添加一个div试试</div>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
